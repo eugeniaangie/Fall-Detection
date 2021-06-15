6ceaddc5725e79b91ed58bfbffbd82360205a69b
Eugenia Angela S <eugeniaangelas@gmail.com>
2021-06-16 03:21:40 +0700

    update

--- capture_dataset.py
+++ capture_dataset.py
@@ -4,19 +4,19 @@ import os
 from datetime import datetime
 
 x=0
-while x <10:
+while x <15:
     cap = cv2.VideoCapture(0)
     # Capture frame-by-frame
     ret, frame = cap.read()
     cap.release()
     #save to directory
-    path = 'D:/You deserve better/1. TA SEMOGA CERIA, 2021 lulus AAMIIN/1. Fall Detection/Dataset/sitting'
+    path = 'D:/You deserve better/1. TA SEMOGA CERIA, 2021 lulus AAMIIN/1. Fall Detection/Dataset/validation/sitting'
     # Our operations on the frame come here
     x=x+1
     current_time = datetime.now()
     image_name = current_time.strftime("%d%H%M%S")
     cv2.imwrite(os.path.join(path, "falling"+image_name+".png"), frame)
-    time.sleep(2)
+    time.sleep(1)
 
 # When everything done, release the capture
 cap.release()
